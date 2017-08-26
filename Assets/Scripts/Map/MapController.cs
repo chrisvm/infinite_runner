d3c36667c3bd38a1b5ca2f5bdbf14548e1144a82
cvelez <christian.velez55@gmail.com>
2017-08-26 17:31:47 -0400

    changed pixelsperunit to 64

--- Assets/Scripts/Map/MapController.cs
+++ Assets/Scripts/Map/MapController.cs
@@ -41,29 +41,39 @@ namespace Map {
 
 		private const int mapWidth = 11;
 		private GameObject boardHolder;
+
+		void Start()
+		{
+			boardHolder = new GameObject();
+			for (var index = 0; index < 3; index++) {
+				var chunk = CreateMapChunk(mapRepresentation, new Vector2(index * mapWidth, 0));
+				chunk.transform.SetParent(boardHolder.transform);
+			}
+		}
 		
-		void Start () {
+		GameObject CreateMapChunk(uint[] representation, Vector2 trans) 
+		{
 			// parent transform for the map
-			boardHolder = new GameObject("Board");
-			boardHolder.transform.localScale.Set(-1, 1, 1);
-			for (var index = 0; index < mapRepresentation.Length; index += 1) {
-				var tileType = (TileType) mapRepresentation[index];
+			var parent = new GameObject("Board");
+			for (var index = 0; index < representation.Length; index += 1) {
+				var tileType = (TileType) representation[index];
 				if (tileType == TileType.Empty) continue;
+				
 				var tileSprite = GetSpriteForTileType(tileType);
 				var xGrid = index % mapWidth * tileSprite.bounds.size.x;
 				var yGrid = index / mapWidth * tileSprite.bounds.size.y;
 				
 				// create the gameobject to hold the created sprite renderer
 				var gameObj = new GameObject();
-				gameObj.transform.position = new Vector3(xGrid, yGrid, 0f);
-				gameObj.transform.SetParent(boardHolder.transform);
+				gameObj.transform.position = new Vector3(xGrid + trans.x, yGrid + trans.y, 0f);
+				gameObj.transform.SetParent(parent.transform);
 				
 				// create the sprite to set to SpriteRenderer
 				var sprite = Instantiate(tileSprite, new Vector3(), Quaternion.identity);
 				var spriteRenderer = gameObj.AddComponent<SpriteRenderer>();
 				spriteRenderer.sprite = sprite;
-				
 			}
+			return parent;
 		}
 	}
 }
