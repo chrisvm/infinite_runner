ff74219dc77ce539213cb36060d0f70c27704139
DESKTOP-6M9EP6R\Gnomo <angelgnomo@gmail.com>
2017-09-03 19:14:34 -0400

    - Fixed font size and added property for screen to be loaded to main menu

---
+++ Assets/Scripts/Gui/MainMenuInputScript.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuInputScript : MonoBehaviour {
+
+    public GameObject startButton;
+    public string GameSceneName;
+
+    private void OnMouseDown()
+    {
+        if (this.tag.Equals(startButton.tag))
+        {
+            SceneManager.LoadScene(GameSceneName);
+        }
+    }
+
+}
